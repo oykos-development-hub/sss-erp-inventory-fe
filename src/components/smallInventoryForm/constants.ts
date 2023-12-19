9c9069c309183bc7014763e711c800a70883a729
oykos-development-hub <hub@oykos.me>
2023-12-19 13:34:16 +0000

    Update code from GitLab

--- src/components/smallInventoryForm/constants.ts
+++ src/components/smallInventoryForm/constants.ts
@@ -2,8 +2,9 @@ import {SmallInventoryAddFormProps} from './types';
 
 export const initialValues: SmallInventoryAddFormProps = {
   invoice_number: '',
-  date_of_purchase: '',
+  date_of_purchase: undefined,
   source: null,
   supplier: null,
   office: null,
+  items: [],
 };
--- src/components/smallInventoryForm/types.ts
+++ src/components/smallInventoryForm/types.ts
@@ -2,7 +2,7 @@ import {DropdownDataNumber, DropdownDataString} from '../../types/dropdownData';
 
 export interface SmallInventoryAddFormProps {
   invoice_number: string;
-  date_of_purchase: Date;
+  date_of_purchase?: Date;
   source: DropdownDataString | null;
   supplier: DropdownDataNumber | null;
   office: DropdownDataNumber | null;
